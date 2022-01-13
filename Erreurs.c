5f131cda21757c8a9b7dffc1f7d613ab615ca2d0
Adam <adam.haissoubi@gmail.com>
2022-01-13 15:02:35 +0100

    réorganisation fichiers et fonctions + 1er bonus (mutlijoueur)
    en attendant l'IA

---
+++ Erreurs.c
@@ -0,0 +1,5 @@
+//
+// Created by Adam HAISSOUBI-VOGIER on 13/01/2022.
+//
+
+#include "Erreurs.h"
---
+++ Erreurs.h
@@ -0,0 +1,8 @@
+//
+// Created by Adam HAISSOUBI-VOGIER on 13/01/2022.
+//
+
+#ifndef BASECODE_ERREURS_H
+#define BASECODE_ERREURS_H
+
+#endif //BASECODE_ERREURS_H
